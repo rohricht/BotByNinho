51cce238715822b65f5950ccb44a1f906aaeb4f8
Nilton Röhricht Junior <rohricht@gmail.com>
2018-03-18 17:57:33 -0300

    Main files
    Main files to understand the code

---
+++ MessagesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BotByNinho.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Connector;
+
+namespace BotByNinho
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            var luisAttributes = new LuisModelAttribute(
+                ConfigurationManager.AppSettings["LuisID"],
+                ConfigurationManager.AppSettings["LuisSubscriptionKey"]);
+            var luisService = new LuisService(luisAttributes);
+
+            switch (activity.Type)
+            {
+                case ActivityTypes.Message:
+                    // If receive a start command from Telegram...
+                    if (activity.Text == "/start")
+                    {
+                        var telegram = activity.CreateReply();
+                        telegram.Text = "Olá, que bom ver você por aqui no Telegram!";
+
+                        await connector.Conversations.ReplyToActivityAsync(telegram);
+                        break;
+                    }
+                    // Send to Luis consideration...
+                    await Conversation.SendAsync(activity, () => new RootDialog(luisService));
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    if (activity.MembersAdded.Any(o => o.Id == activity.Recipient.Id))
+                    {
+                        var reply = activity.CreateReply();
+                        reply.Text = "Olá, que bom ver você por aqui!";
+
+                        await connector.Conversations.ReplyToActivityAsync(reply);
+                    }
+                    break;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RootDialog.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using BotByNinho.Services;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+
+namespace BotByNinho.Dialogs
+{
+    [Serializable]
+    public class RootDialog : LuisDialog<object>
+    {
+        private enum dlgType { greeting, emotion, translate };
+
+        public RootDialog(ILuisService service) : base(service) { }
+
+        [LuisIntent("None")]
+        public async Task NoneAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Desculpe, não entendi o que você me disse. Consegue me explicar melhor?");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("")]
+        public async Task UnkknownAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Ops, não reconheci o que você me disse... Consegue me explicar melhor?");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("Greetings")]
+        public async Task GreetingAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Oi! Hoje estou muito bem... espero que com você também esteja tudo ótimo!");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("Consciousness")]
+        public async Task ConsciousAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Bom, eu sou um robô aprendendo a conversar com humanos. O Nilton é meu mestre!");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("Personal")]
+        public async Task PersonalAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Cara, sou carinhosamente conhecido como NinhoBot :D");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("Gratitude")]
+        public async Task GratitudeAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("de nada...");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("Translate")]
+        public async Task TranslateAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Vamos traduzir algumas coisas então! Aliás, no momento eu só sei inglês ;D" + 
+                                    "\n É só me dizer o que você deseja traduzir...");
+            context.Wait(TranslatePtBr);
+        }
+
+        [LuisIntent("Emotion")]
+        public async Task EmotionAsync(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Então você quer falar sobre emoções... No momento estou aprendendo sobre isso.\n" + 
+                                    "Não é tão simples assim entender as emoções humanas...");
+            context.Done<string>(null);
+        }
+
+        #region Functions
+
+        private async Task TranslatePtBr(IDialogContext context, IAwaitable<IMessageActivity> value)
+        {
+            var msg = await value;
+            var txt = msg.Text;
+
+            var response = await new Translation().TranslateAsync(txt);
+
+            await context.PostAsync(response);
+            context.Wait(MessageReceived);
+        }
+
+        #endregion
+    }
+}
